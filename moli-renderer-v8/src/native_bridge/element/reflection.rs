6acdc06caba5be91c6fa780e3238bef314bb5b83
ldm0 <ldm2993593805@163.com>
2026-08-30 15:26:32 +0800

    fix(html): remove obsolete area metadata reflectors

--- moli-renderer-v8/src/native_bridge/element/reflection.rs
+++ moli-renderer-v8/src/native_bridge/element/reflection.rs
@@ -174,10 +174,8 @@ pub(super) enum DomStringReflection {
     AreaAlt,
     AreaCoords,
     AreaDownload,
-    AreaHreflang,
     AreaReferrerPolicy,
     AreaShape,
-    AreaType,
     BrClear,
     DataValue,
     EmbedHeight,
@@ -353,10 +351,6 @@ const DOM_STRING_REFLECTION_DESCRIPTORS: &[(DomStringReflection, DomStringReflec
             DomStringReflectionDescriptor::new("HTMLAreaElement", "download", "download"),
         ),
         (
-            DomStringReflection::AreaHreflang,
-            DomStringReflectionDescriptor::new("HTMLAreaElement", "hreflang", "hreflang"),
-        ),
-        (
             DomStringReflection::AreaReferrerPolicy,
             DomStringReflectionDescriptor::new(
                 "HTMLAreaElement",
@@ -367,10 +361,6 @@ const DOM_STRING_REFLECTION_DESCRIPTORS: &[(DomStringReflection, DomStringReflec
         (
             DomStringReflection::AreaShape,
             DomStringReflectionDescriptor::new("HTMLAreaElement", "shape", "shape"),
-        ),
-        (
-            DomStringReflection::AreaType,
-            DomStringReflectionDescriptor::new("HTMLAreaElement", "type", "type"),
         ),
         (
             DomStringReflection::BrClear,
