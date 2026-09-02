326294f5a595a4823527dc3cd3be8b908989ca9d
ldm0 <ldm2993593805@163.com>
2026-09-02 16:25:17 +0800

    fix(v8): use boolean access handler callbacks

---
+++ moli-v8-test-util/tests/object_template_bindings.rs
@@ -0,0 +1,151 @@
+use std::pin::pin;
+
+fn eval<'s>(scope: &mut v8::PinScope<'s, '_>, source: &str) -> v8::Local<'s, v8::Value> {
+    let source = v8::String::new(scope, source).expect("test source should fit in a V8 string");
+    let script = v8::Script::compile(scope, source, None).expect("test source should compile");
+    script.run(scope).expect("test source should run")
+}
+
+unsafe extern "C" fn deny_cross_context_access(
+    _accessing_context: v8::Local<'_, v8::Context>,
+    _accessed_object: v8::Local<'_, v8::Object>,
+    _data: v8::Local<'_, v8::Value>,
+) -> bool {
+    false
+}
+
+fn miss_named_get<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _key: v8::Local<'s, v8::Name>,
+    _args: v8::PropertyCallbackArguments<'s>,
+    _rv: v8::ReturnValue<v8::Value>,
+) -> v8::Intercepted {
+    v8::Intercepted::kNo
+}
+
+fn miss_indexed_get<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _index: u32,
+    _args: v8::PropertyCallbackArguments<'s>,
+    _rv: v8::ReturnValue<v8::Value>,
+) -> v8::Intercepted {
+    v8::Intercepted::kNo
+}
+
+fn reject_named_set<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _key: v8::Local<'s, v8::Name>,
+    _value: v8::Local<'s, v8::Value>,
+    _args: v8::PropertyCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<v8::Boolean>,
+) -> v8::Intercepted {
+    rv.set_bool(false);
+    v8::Intercepted::kYes
+}
+
+fn reject_indexed_set<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _index: u32,
+    _value: v8::Local<'s, v8::Value>,
+    _args: v8::PropertyCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<v8::Boolean>,
+) -> v8::Intercepted {
+    rv.set_bool(false);
+    v8::Intercepted::kYes
+}
+
+fn reject_named_define<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _key: v8::Local<'s, v8::Name>,
+    _descriptor: &v8::PropertyDescriptor,
+    _args: v8::PropertyCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<v8::Boolean>,
+) -> v8::Intercepted {
+    rv.set_bool(false);
+    v8::Intercepted::kYes
+}
+
+fn reject_indexed_define<'s>(
+    _scope: &mut v8::PinScope<'s, '_>,
+    _index: u32,
+    _descriptor: &v8::PropertyDescriptor,
+    _args: v8::PropertyCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<v8::Boolean>,
+) -> v8::Intercepted {
+    rv.set_bool(false);
+    v8::Intercepted::kYes
+}
+
+#[test]
+fn denied_access_handlers_preserve_boolean_setter_results() {
+    moli_v8_test_util::ensure_v8();
+    let mut isolate = v8::Isolate::new(v8::CreateParams::default());
+    let scope = pin!(v8::HandleScope::new(&mut isolate));
+    let scope = &mut scope.init();
+
+    let owner_template = v8::ObjectTemplate::new(scope);
+    owner_template.set_security_token_access_check_and_handlers(
+        deny_cross_context_access,
+        v8::NamedPropertyHandlerConfiguration::new()
+            .getter(miss_named_get)
+            .setter(reject_named_set)
+            .definer(reject_named_define),
+        v8::IndexedPropertyHandlerConfiguration::new()
+            .getter(miss_indexed_get)
+            .setter(reject_indexed_set)
+            .definer(reject_indexed_define),
+    );
+    let owner_context = v8::Context::new(
+        scope,
+        v8::ContextOptions {
+            global_template: Some(owner_template),
+            ..Default::default()
+        },
+    );
+    let caller_context = v8::Context::new(
+        scope,
+        v8::ContextOptions {
+            global_template: Some(owner_template),
+            ..Default::default()
+        },
+    );
+    let owner_token = v8::String::new(scope, "owner-token").unwrap();
+    owner_context.set_security_token(owner_token.into());
+    let caller_token = v8::String::new(scope, "caller-token").unwrap();
+    caller_context.set_security_token(caller_token.into());
+    let target = v8::Global::new(scope, owner_context.global(scope));
+
+    let caller_scope = &mut v8::ContextScope::new(scope, caller_context);
+    let target = v8::Local::new(caller_scope, &target);
+    let target_key = v8::String::new(caller_scope, "target").unwrap();
+    assert_eq!(
+        caller_context
+            .global(caller_scope)
+            .set(caller_scope, target_key.into(), target.into()),
+        Some(true)
+    );
+
+    let result = eval(
+        caller_scope,
+        r#"
+JSON.stringify([
+  Reflect.set(target, "name", 1),
+  Reflect.set(target, 0, 1),
+  (() => {
+    "use strict";
+    try { target.strictName = 1; return false; }
+    catch (error) { return error instanceof TypeError; }
+  })(),
+  (() => {
+    "use strict";
+    try { target[2] = 1; return false; }
+    catch (error) { return error instanceof TypeError; }
+  })()
+])
+"#,
+    );
+    assert_eq!(
+        result.to_rust_string_lossy(caller_scope),
+        "[false,false,true,true]"
+    );
+}
